fc65be9b19c3e510b282f6fe4f337cf30fe32945
Supun Kandaudahewa <skandaudahewa@gmail.com>
2021-09-22 21:45:29 -0400

    Models created

---
+++ UniversityApp/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniversityApp.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
